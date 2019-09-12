04a60152769647348b2e664503dd630ca1a21214
a1573595 <a1573595@gmail.com>
2019-09-12 23:37:48 +0800

    add BaseActivity、request camera permission、set screen orientation landscape

--- app/src/main/java/com/a1573595/realtimecamera/MainActivity.java
+++ app/src/main/java/com/a1573595/realtimecamera/MainActivity.java
@@ -1,14 +1,18 @@
 package com.a1573595.realtimecamera;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+    private final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
+    private final int REQUEST_CAMERA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(!hasPermissions(PERMISSION_CAMERA))
+            requestPermission(REQUEST_CAMERA, PERMISSION_CAMERA);
     }
 }
